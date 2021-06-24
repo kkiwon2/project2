a4929219123014e5aca50d9cf4ae11020709683a
kkiwo <kkiwon2>
2021-06-24 20:44:31 +0900

    init 0624 (Travis CI와 S3연동 및 CodeDeply까지)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -24,7 +24,7 @@ fi
 
 echo "> 새 어플리케이션 배포"
 
-JAR_NAME=$(ls -tr $REPOSITORY/ | grep jar | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
@@ -37,5 +37,5 @@ echo "> $JAR_NAME 실행"
 
 nohup java -jar \
     -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties \
-    $REPOSITORY/$JAR_NAME 2>&1 &
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
 
